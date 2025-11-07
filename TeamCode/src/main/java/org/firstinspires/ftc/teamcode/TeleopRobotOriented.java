3a743cf59fa1e9eaffbe7278e94988c26cc52cf4
user-e6b6 <user.e6b6@gmail.com>
2025-11-06 19:41:56 -0600

    working code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopRobotOriented.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopRobotOriented.java
@@ -1,12 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "Starter Teleop", group = "Starter")
 public class TeleopRobotOriented extends LinearOpMode {
+
+    final double LAUNCHER_TARGET_VELOCITY = 1125;
+    final double LAUNCHER_MIN_VELOCITY = 1075;
+    private DcMotorEx launcher = null;
+
+    ElapsedTime feederTimer = new ElapsedTime();
+
+    final double FEED_TIME_SECONDS = 0.20; //The feeder servos run this long when a shot is requested.
+    final double STOP_SPEED = 0.0; //We send this power to the servos when we want them to stop.
+    final double FULL_SPEED = 1.0;
+
+    private enum LaunchState {
+        IDLE,
+        SPIN_UP,
+        LAUNCH,
+        LAUNCHING,
+    }
+
+    private TeleopRobotOriented.LaunchState launchState;
+
+    // Setup a variable for each drive wheel to save power level for telemetry
+    double leftPower;
+    double rightPower;
+
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors
@@ -16,6 +45,9 @@ public class TeleopRobotOriented extends LinearOpMode {
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
 
+        launcher = hardwareMap.get(DcMotorEx.class, "launcher");
+        launcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         // Reverse the right side motors. This may be wrong for your setup.
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
@@ -24,6 +56,9 @@ public class TeleopRobotOriented extends LinearOpMode {
         backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        launcher.setZeroPowerBehavior(BRAKE);
+        launcher.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(300, 0, 0, 10));
 
 
         waitForStart();
@@ -48,6 +83,43 @@ public class TeleopRobotOriented extends LinearOpMode {
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
+
+            if (gamepad1.y) {
+                launcher.setVelocity(LAUNCHER_TARGET_VELOCITY);
+            } else if (gamepad1.b) { // stop flywheel
+                launcher.setVelocity(STOP_SPEED);
+            }
+            launch(gamepad1.rightBumperWasPressed());
+        }
+    }
+
+    void launch(boolean shotRequested) {
+        switch (launchState) {
+            case IDLE:
+                if (shotRequested) {
+                    launchState = TeleopRobotOriented.LaunchState.SPIN_UP;
+                }
+                break;
+            case SPIN_UP:
+                launcher.setVelocity(LAUNCHER_TARGET_VELOCITY);
+                if (launcher.getVelocity() > LAUNCHER_MIN_VELOCITY) {
+                    launchState = TeleopRobotOriented.LaunchState.LAUNCH;
+                }
+                break;
+            case LAUNCH:
+                //leftFeeder.setPower(FULL_SPEED);
+                // rightFeeder.setPower(FULL_SPEED);
+                // feederTimer.reset();
+                launchState = TeleopRobotOriented.LaunchState.LAUNCHING;
+                break;
+            case LAUNCHING:
+                if (feederTimer.seconds() > FEED_TIME_SECONDS) {
+                    launchState = TeleopRobotOriented.LaunchState.IDLE;
+                    // leftFeeder.setPower(STOP_SPEED);
+                    //  rightFeeder.setPower(STOP_SPEED);
+                    //  }
+                    break;
+                }
         }
     }
 }

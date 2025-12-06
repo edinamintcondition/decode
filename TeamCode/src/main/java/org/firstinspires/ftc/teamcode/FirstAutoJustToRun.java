4015a1b12b9b7303eb5085003c85651fa16fcf4d
user-e6b6 <user.e6b6@gmail.com>
2025-12-06 14:01:07 -0600

    changed names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstAutoJustToRun.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstAutoJustToRun.java
@@ -54,7 +54,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name = "FirstAutoJustToRun", group = "Test")
+@Autonomous(name = "FirstAuto10Inches", group = "Test")
 //@Disabled
 public class FirstAutoJustToRun extends LinearOpMode {
 

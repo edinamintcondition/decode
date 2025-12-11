b84999cbc57341e0b83da5af33fbce1e5e712ca8
user-e6b6 <user.e6b6@gmail.com>
2025-12-11 17:42:43 -0600

    Update StarterBotTeleopElliot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleopElliot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleopElliot.java
@@ -318,15 +318,14 @@ public class StarterBotTeleopElliot extends OpMode {
 
         // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
         double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-        double lateral =  gamepad1.right_stick_x;
         double yaw     =  gamepad1.left_stick_x;
 
         // Combine the joystick requests for each axis-motion to determine each wheel's power.
         // Set up a variable for each drive wheel to save the power level for telemetry.
-        double frontLeftPower  = axial + lateral + yaw;
-        double frontRightPower = axial - lateral - yaw;
-        double backLeftPower   = axial - lateral + yaw;
-        double backRightPower  = axial + lateral - yaw;
+        double frontLeftPower  = axial + yaw;
+        double frontRightPower = axial - yaw;
+        double backLeftPower   = axial + yaw;
+        double backRightPower  = axial - yaw;
 
         // Normalize the values so no wheel power exceeds 100%
         // This ensures that the robot maintains the desired motion.
@@ -360,7 +359,10 @@ public class StarterBotTeleopElliot extends OpMode {
         telemetry.addData("Back  left/Right", "%4.2f, %4.2f", backLeftPower, backRightPower);
         telemetry.addData("Speed", launcher.getVelocity());
         telemetry.update();
+
+
     }
+
         
     /*
     void launch(boolean shotRequested) {

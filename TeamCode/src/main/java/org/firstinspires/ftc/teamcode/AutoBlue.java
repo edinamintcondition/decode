0561a8d375adbe2acc249127557a1d4c2dab8024
ishwaqo8120 <ishmomar14@gmail.com>
2025-12-13 06:40:34 -0600

    Make some changes to get AutoBlue to work
    I made some changes to AutoBlue because I noticed it wouldn't run. Namely, I added somethings in the init method, added the state machine in the autonomousPathUpdate method, and overall restructured things for them to be more in line with the example auto.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlue.java
@@ -6,37 +6,52 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-    @Autonomous(name = "Pedro Pathing Autonomous", group = "Autonomous")
+import java.nio.file.Paths;
+
+@Autonomous(name = "[BLUE]Pedro Pathing Autonomous", group = "Autonomous")
     @Configurable // Panels
     public class AutoBlue extends OpMode {
 
         private TelemetryManager panelsTelemetry; // Panels Telemetry instance
+        private Timer pathTimer, actionTimer, opmodeTimer;
         public Follower follower; // Pedro Pathing follower instance
         private int pathState; // Current autonomous path state (state machine)
         private Paths paths; // Paths defined in the Paths class
+        public PathChain Path1;
+        private Pose startPose = new Pose(56.000, 8.000);
 
         @Override
         public void init() {
             panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
 
             follower = Constants.createFollower(hardwareMap);
-            follower.setStartingPose(new Pose(72, 8, Math.toRadians(90)));
-
-            paths = new Paths(follower); // Build paths
+            follower.setStartingPose(startPose);
 
             panelsTelemetry.debug("Status", "Initialized");
             panelsTelemetry.update(telemetry);
+
+                pathTimer = new Timer();
+                opmodeTimer = new Timer();
+                opmodeTimer.resetTimer();
+
+                buildPaths(follower);
+        }
+        public void setPathState(int pState) {
+            pathState = pState;
+            pathTimer.resetTimer();
         }
 
         @Override
         public void loop() {
             follower.update(); // Update Pedro Pathing
-            pathState = autonomousPathUpdate(); // Update autonomous state machine
+            autonomousPathUpdate();
 
             // Log values to Panels and Driver Station
             panelsTelemetry.debug("Path State", pathState);
@@ -46,25 +61,25 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
             panelsTelemetry.update(telemetry);
         }
 
-        public static class Paths {
-
-            public PathChain Path1;
-
-            public Paths(Follower follower) {
+            public void buildPaths(Follower follower) {
                 Path1 = follower
                         .pathBuilder()
                         .addPath(
-                                new BezierLine(new Pose(56.000, 8.000), new Pose(48.585, 95.415))
+                                new BezierLine(startPose, new Pose(48.585, 95.415))
                         )
                         .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(145))
                         .build();
             }
-        }
 
-        public int autonomousPathUpdate() {
+        public void autonomousPathUpdate() {
             // Add your state machine Here
             // Access paths with paths.pathName
             // Refer to the Pedro Pathing Docs (Auto Example) for an example state machine
-            return pathState;
+            switch(pathState) {
+                case 0:
+                    follower.followPath(Path1);
+                    setPathState(-1);
+
+            }
         }
     }
